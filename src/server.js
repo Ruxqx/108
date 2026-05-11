813ee0eb0bb4afbeea0e61940ee6cca21824c416
Joseph Albert Delos Reyes Jr <josephalbertdelosreyes@Josephs-MacBook-Pro-3.local>
2026-05-10 23:15:44 -0700

    removed double port list

--- src/server.js
+++ src/server.js
@@ -12,9 +12,7 @@ const chatRoutes = require("./routes/chat");
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+
 
 app.set("trust proxy", 1);
 app.use(
@@ -59,7 +57,7 @@ app.use((error, request, response, next) => {
 });
 
 initDatabase().then(() => {
-  app.listen(port, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Blackjack app running on http://localhost:${port}`);
   });
 });
